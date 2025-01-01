1e837624c3c3b6beb6a1bbf90679f1084ca94a6d
Gonzalo Meana Balseiro <gonzalomeanabalseiro@gmail.com>
2025-01-01 18:43:18 +0900

    implement delta time vector

--- src/Game.cpp
+++ src/Game.cpp
@@ -73,7 +73,7 @@ glm::vec2 playerVelocity;
 void Game::Setup()
 {
     playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(0.5, 0.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 // this function updates the game objects
@@ -85,12 +85,14 @@ void Game::Update()
     {
         SDL_Delay(timeToWait);
     }
+    // the diferece in ticks from last frame converted to seconds
+    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
 
     // store the current time in milliseconds
     millisecsPreviousFrame = SDL_GetTicks();
 
-    playerPosition.x += playerVelocity.x;
-    playerPosition.y += playerVelocity.y;
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 // this function renders the game objects
